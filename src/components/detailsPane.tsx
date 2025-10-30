2e7264bc39b42376efe066638678ac4662991b08
kyliecrump-kiwirail <kylie.crump@kiwirail.co.nz>
2025-10-30 21:00:21 +1300

    Added additional details to project details pane

--- src/components/detailsPane.tsx
+++ src/components/detailsPane.tsx
@@ -10,7 +10,10 @@ type HoverBarProps = {
 
 const defaultProjectDetails = {
     projectTitle: "Welcome",
-    projectDescription: "Click on any project to show details" 
+    projectCategory: "Click on any project to show details",
+    projectStartDate: "",
+    projectEndDate: "",
+    projectDescription: ""
 }
 
 export default function DetailsPane(currentMap:HoverBarProps)
@@ -21,31 +24,37 @@ export default function DetailsPane(currentMap:HoverBarProps)
         const getMap = currentMap.map
         if (!getMap) return 
         getMap.on('click', 'point', (e) => {
-                if (e.features) 
-                {
-                    getMap.setFeatureState(
-                        { source: 'my-data', id: e.features[0].id },
-                        { clicked: true }
-                    );
-                    setClickedProjectDetails({
-                        projectTitle: e.features[0].properties['ProjectName'],
-                        projectDescription: e.features[0].properties['ProjectDescription']
-                    })
-                    const polygon = (e.features[0].geometry as Polygon)
-                    const coordinates = centroid(polygon)
+            if (e.features) 
+            {
+                getMap.setFeatureState(
+                    { source: 'my-data', id: e.features[0].id },
+                    { clicked: true }
+                );
+                console.log(e.features[0].properties)
+                setClickedProjectDetails({
+                    projectTitle: e.features[0].properties['ProjectName'],
+                    projectCategory: e.features[0].properties['ProjectCategory'],
+                    projectDescription: e.features[0].properties['ProjectDescription'],
+                    projectStartDate: new Date(e.features[0].properties['ProjectStartDate']).toLocaleDateString(),
+                    projectEndDate: new Date(e.features[0].properties['ProjectEndDate']).toLocaleDateString()
+                })
+                const polygon = (e.features[0].geometry as Polygon)
+                const coordinates = centroid(polygon)
 
-                    getMap.flyTo({
-                    center: [coordinates[0], coordinates[1]],
-                    zoom: 15
-                    });
-                }
-            });
+                getMap.flyTo({
+                center: [coordinates[0], coordinates[1]],
+                zoom: 15
+                });
+            }
+        });
     })
 
     return (
         <Box  sx={{padding: "2em", width:"20em", height:"50em", bgcolor: "#FFFF", zIndex: 10, position: 'absolute'}}>
             <Typography paddingBottom="1em" variant="h6">{clickedProjectDetails.projectTitle}</Typography>
-            <Typography>{clickedProjectDetails.projectDescription}</Typography>
+            <Typography paddingBottom="1em" fontWeight="bold">{clickedProjectDetails.projectCategory}</Typography>
+            <Typography paddingBottom="1em" >{clickedProjectDetails.projectStartDate} - {clickedProjectDetails.projectEndDate}</Typography>
+            <Typography paddingBottom="1em" >{clickedProjectDetails.projectDescription}</Typography>
         </Box>
     )
 }
